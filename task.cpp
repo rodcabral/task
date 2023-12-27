84c504f5213abb43c6ceb3bc11120e9c8fd43001
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2023-12-27 12:58:35 -0300

    feat: save the task file in the user folder

--- task.cpp
+++ task.cpp
@@ -6,27 +6,28 @@
 int main(int argc, char**argv) {
     std::fstream taskFile;
 
+    std::string relativePathToFile = std::string(std::getenv("HOME")) + "/.task";
+
     if(argc < 2) {
-        if(!std::filesystem::exists(".task")) {
+        if(!std::filesystem::exists(relativePathToFile)) {
             std::cout << "You don't have any task!\n";
-            return 0;
+        } else {
+            taskFile.open(relativePathToFile, std::ios::in);
+
+            std::cout << "\nCurrent Task: \e[0;92m" << taskFile.rdbuf() << "\n\n\e[0;39m";
+
+            taskFile.close();
         }
-
-        taskFile.open(".task", std::ios::in);
-
-        std::cout << "\nCurrent Task: \e[0;92m" << taskFile.rdbuf() << "\n\n\e[0;39m";
-
-        taskFile.close();
         return 0;
     }
 
     if(std::strncmp(argv[1], "complete", 10) == 0) {
-        remove(".task");
+        remove(relativePathToFile.c_str());
         std::cout << "Task completed!\n";
         return 0;
     }
 
-    taskFile.open(".task", std::ios::out);
+    taskFile.open(relativePathToFile, std::ios::out);
 
     taskFile << argv[1];
 
