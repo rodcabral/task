6ea5155a08ed3581079e5ba998aa8f82a8f82ea6
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2023-12-27 21:00:22 -0300

    feat: create history system

--- task.cpp
+++ task.cpp
@@ -8,6 +8,7 @@ int main(int argc, char**argv) {
 
     std::string relativePath = std::string(std::getenv("HOME"));
     std::string task = relativePath + "/.task";
+    std::string taskHistory = relativePath + "/.taskHistory";
 
     if(argc < 2) {
         if(!std::filesystem::exists(task)) {
@@ -23,9 +24,43 @@ int main(int argc, char**argv) {
     }
 
     if(std::strncmp(argv[1], "complete", 10) == 0) {
+
+        if(!std::filesystem::exists(task)) {
+            std::cout << "You don't have any task!\n";
+            return 0;
+        }
+
+        taskFile.open(task, std::ios::in);
+
+        std::string taskContent;
+
+        std::getline(taskFile, taskContent);
+
+        taskFile.close();
+
+        taskFile.open(taskHistory, std::ios::app);
+
+        taskFile << "\n" << taskContent << "\e[0;92m\t[Completed]\e[0;39m\n\n";
+
+        taskFile.close();
+
         remove(task.c_str());
         std::cout << "Task completed!\n";
-        
+        return 0;
+    }
+
+    if(std::strncmp(argv[1], "history", 9) == 0) {
+        if(!std::filesystem::exists(taskHistory)) {
+            std::cout << "You have not created any tasks!\n";
+            return 0;
+        }
+ 
+        taskFile.open(taskHistory, std::ios::in);
+
+        std::cout << taskFile.rdbuf();
+
+        taskFile.close();
+
         return 0;
     }
 
