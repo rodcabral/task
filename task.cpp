1a9a12b077ee11f1aad25e7a6565f83fa1a7baca
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2023-12-27 22:13:33 -0300

    feat: functionality of delete the current task and all history

--- task.cpp
+++ task.cpp
@@ -24,7 +24,6 @@ int main(int argc, char**argv) {
     }
 
     if(std::strncmp(argv[1], "complete", 10) == 0) {
-
         if(!std::filesystem::exists(task)) {
             std::cout << "You don't have any task!\n";
             return 0;
@@ -67,6 +66,27 @@ int main(int argc, char**argv) {
     if(std::strncmp(argv[1], "remove", 8) == 0) {
         remove(task.c_str());
         std::cout << "Removed!\n";
+        return 0;
+    }
+
+    if(std::strncmp(argv[1], "delete", 8) == 0) {
+        std::cout << "Are you sure you want to delete\nthe current task and all history? [y/n]: ";
+
+        std::string str = "";
+
+        while(1) {
+            std::getline(std::cin, str);
+
+            if(str != "" && str != "y") {
+                break;
+            }
+
+            remove(task.c_str());
+            remove(taskHistory.c_str());
+            std::cout << "Deleted!\n";   
+            break;
+        }
+
         return 0;
     }
 
